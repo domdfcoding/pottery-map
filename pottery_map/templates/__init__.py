3d6e5fd9d72041ed33f40dd7fd3e15447535c0fd
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-02-05 08:12:52 +0000

    Use fork of jinja2-workarounds

--- pottery_map/templates/__init__.py
+++ pottery_map/templates/__init__.py
@@ -30,9 +30,9 @@ Jinja2 templates.
 import jinja2
 from domdf_python_tools.paths import PathPlus
 from jinja2 import Environment
+from jinja2_workarounds import MultiLineInclude  # type: ignore[import-untyped]
 
 # this package
-from pottery_map._jinja2_workarounds import MultiLineInclude
 from pottery_map.utils import make_id
 
 templates = Environment(  # nosec: B701
