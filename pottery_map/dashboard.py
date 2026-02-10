4381032d1075e9a9465a40a8c8b7f083bb9cf748
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-02-10 21:45:55 +0000

    Finalise first two charts

--- pottery_map/dashboard.py
+++ pottery_map/dashboard.py
@@ -32,12 +32,12 @@ from typing import Any
 
 # 3rd party
 import networkx
+from gradpyent import Gradient
 
 # this package
 from pottery_map.companies import Companies, _get_item_count
 
 __all__ = ["colour_cycle", "companies_bar_chart", "groups_pie_chart"]
-
 
 colour_cycle = [
 		"blue",
@@ -79,7 +79,7 @@ def groups_pie_chart(companies: Companies) -> dict[str, Any]:
 	other_count = 0
 	company_counts = {}
 	for company in companies.top_level_companies:
-		ancestors = networkx.ancestors(companies.graph, company)
+		ancestors: set[str] = networkx.ancestors(companies.graph, company)
 		ancestors.add(company)
 		item_count = _get_item_count(companies.company_item_counts, ancestors)
 		if item_count > 1:
@@ -99,7 +99,8 @@ def groups_pie_chart(companies: Companies) -> dict[str, Any]:
 			"datasets": [{
 					"data": list(sorted_company_counts.values()),
 					"backgroundColor": colour_cycle,
-					"borderColor": "#fff",
+					"borderColor": "#8b8680",
+					"borderWidth": 1,
 					}],
 			}
 
@@ -118,17 +119,30 @@ def companies_bar_chart(companies: Companies) -> dict[str, Any]:
 	print(companies.company_item_counts)
 
 	sorted_item_counts = dict(sorted(
-		companies.company_item_counts.items(),
-		key=itemgetter(1),
-		reverse=True,
-	))
+			companies.company_item_counts.items(),
+			key=itemgetter(1),
+			reverse=True,
+			))
+
+	unique_counts = sorted(set(companies.company_item_counts.values()))
+	step = 1 / (len(unique_counts) - 1)
+	input_list = [0 + (step * x) for x in range(len(unique_counts))]
+
+	gg = Gradient(gradient_start="#0000FF", gradient_end="#00FF00")
+	count_colour_map = dict(zip(unique_counts, gg.get_gradient_series(series=input_list, fmt="html")))
+
+	data = []
+	background_colour = []
+
+	for count in sorted_item_counts.values():
+		data.append(count)
+		background_colour.append(count_colour_map[count])
 
 	companies_bar_chart_data = {
-			"labels":
-					list(sorted_item_counts.keys()),
+			"labels": list(sorted_item_counts.keys()),
 			"datasets": [{
-					"data": list(sorted_item_counts.values()),
-					"backgroundColor": colour_cycle,
+					"data": data,
+					"backgroundColor": background_colour,
 					"borderColor": "#fff",
 					}],
 			}
