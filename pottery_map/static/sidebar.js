6f2f10063e9cf977237d57b256f9f07ed9048444
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-04-24 09:00:24 +0100

    Add company search control to sidebar.

--- pottery_map/static/sidebar.js
+++ pottery_map/static/sidebar.js
@@ -1,11 +1,51 @@
 const toggleButton = document.getElementById('toggle-btn');
 const sidebar = document.getElementById('sidebar');
 
+function getCompanyName(li) {
+	return li.dataset.company;
+}
+
+const fuzzySearch = createFuzzySearch([...companiesMenu.querySelectorAll('li[data-company]')], {
+	getText: (item) => [getCompanyName(item)],
+});
+
+function filterCompanies(query) {
+	// console.log("Search query:", query);
+	// console.log("Results:", fuzzySearch(query))
+	let results = [];
+	fuzzySearch(query).forEach((item) => {
+		const li = item.item;
+		results.push(li);
+	});
+
+	companiesMenu.querySelectorAll('li[data-company]').forEach((li) => {
+		if (results.includes(li)) {
+			li.classList.remove('d-none');
+		} else {
+			li.classList.add('d-none');
+		}
+	});
+}
+
 function setupSidebar() {
 	if (sidebar.classList.contains('close')) {
 		toggleButton.classList.add('rotate');
 	} else {
 		toggleButton.classList.remove('rotate');
+	}
+
+	companiesSearch.addEventListener('input', (e) => {
+		filterCompanies(companiesSearch.value);
+	});
+	companiesSearchForm.addEventListener('reset', (e) => {
+		console.log('Form reset');
+		companiesMenu.querySelectorAll('li[data-company]').forEach((li) => {
+			li.classList.remove('d-none');
+		});
+	});
+
+	if (companiesSearch.value !== '') {
+		filterCompanies(companiesSearch.value);
 	}
 }
 
